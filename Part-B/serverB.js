500e488699286b21e035f5152f758cf40d4e55f7
Lioz Akirav <liozakirav@Liozs-MacBook-Pro.local>
2022-04-12 03:23:56 +0300

    add docker-compose-redis file, add docker file to consumeFromKafka , add get func to the modle of part-b

--- Part-B/serverB.js
+++ Part-B/serverB.js
@@ -25,11 +25,11 @@ app.set('views', __dirname + '/views');
 //where layouts files will be
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
-// app.get('/', function (req, res) {
-//     res.redirect("/pages/dashboard.html")
-// })
-//where style files will be
-// app.use('/', express.static('./views/dashboard'))
+app.get('/', function (req, res) {
+    res.redirect("/pages/dashboard.html")
+})
+// where style files will be
+app.use('/', express.static('./views/dashboard'))
 
 
 // app.use('/', controllerRouter);
@@ -38,39 +38,39 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/insertData.html');
 })
 
-app.post("/", async (req, res) => {
+// app.post("/", async (req, res) => {
 
-    const name = req.body.fname;
+//     const name = req.body.fname;
 
-    (async () => {
-        const client = createClient();
+//     (async () => {
+//         const client = createClient();
 
-        client.on('error', (err) => console.log('Redis Client Error', err));
+//         client.on('error', (err) => console.log('Redis Client Error', err));
 
-        await client.connect();
+//         await client.connect();
 
-           await client.HSET('user2', {
-                'date': '25.01.22',
-                'time': '09:45',
-                'firstName': 'Dan',
-                'lastName': 'Biton',
-                'City': 'TLV',
-                'gender': 'male',
-                'age': '42',
-                'prevCalls': '10',
-                'topic': 'complain'
-            });
+//            await client.HSET('user2', {
+//                 'date': '25.01.22',
+//                 'time': '09:45',
+//                 'firstName': 'Dan',
+//                 'lastName': 'Biton',
+//                 'City': 'TLV',
+//                 'gender': 'male',
+//                 'age': '42',
+//                 'prevCalls': '10',
+//                 'topic': 'complain'
+//             });
 
-        const obj = await client.HGETALL('user2');
-        console.log("user2: " + JSON.stringify(obj));
+//         const obj = await client.HGETALL('user2');
+//         console.log("user2: " + JSON.stringify(obj));
 
-        const keys = await client.keys('*');
-        console.log("keys: " + JSON.stringify(keys));
+//         const keys = await client.keys('*');
+//         console.log("keys: " + JSON.stringify(keys));
 
-    })();
+//     })();
 
-    res.send(name)
-});
+//     res.send(name)
+// });
 
 
 const myPort = process.env.PORT || 2000; // can set PORT to be other num (By the command: set PORT=number)
