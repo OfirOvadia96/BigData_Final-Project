b4a30dd42c9b074f9e9161748a8bb69ddd88c4ae
Lior <lior4007@gmail.com>
2022-04-15 15:37:01 +0300

    Fix predict

--- Part-C/bigML/bml.js
+++ Part-C/bigML/bml.js
@@ -10,7 +10,7 @@ require("../MongoDB/exportDB");
 var connection = new bigml.BigML('LIORATIYA','4084760438bee80f1b62c41d3ffaeeb7f3eb7751')
 
 var source = new bigml.Source(connection);
-source.create('../MongoDB/callDetails.csv', function(error, sourceInfo) {
+source.create('callDetails.csv', function(error, sourceInfo) {
   if (!error && sourceInfo) {
     var dataset = new bigml.Dataset(connection);
     dataset.create(sourceInfo, function(error, datasetInfo) {
