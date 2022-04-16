775f815ce226c6c844a80bc03acc6d1229b09f45
Lioz Akirav <liozakirav@Liozs-MacBook-Pro.local>
2022-04-16 15:48:21 +0300

    fix dynamic object insert to Redis

--- Part-B/models/model.js
+++ Part-B/models/model.js
@@ -3,12 +3,11 @@ const axios = require('axios')
 const cors = require('cors')
 const redisClient = require("./init_redis")
 
-// data expiration time 
-const todayEnd = new Date().setHours(23, 59, 59, 999);
-
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+// data expiration time 
+const todayEnd = new Date().setHours(23, 59, 59, 999);
 
 
 // getting all the photos
@@ -18,17 +17,6 @@ app.get("/photos", async (req, res) => {
     "https://jsonplaceholder.typicode.com/photos",
     { params: { albumId }}
   )
-  
-  let keyName = "key"
-
-  redisClient.hmset(keyName, {
-  'javascript': 'ReactJS',
-  'css': 'TailwindCSS',
-  'node': 'Express'
-  });         
-  // sets an expiration date for the data 
-  redisClient.expireat(keyName, parseInt(todayEnd/1000));
-
   res.json(data)
 })
 
@@ -38,10 +26,21 @@ app.get("/photos/:id", async (req, res) => {
     `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
   )
 
-  redisClient.setex('photos', DEFAULT_EXPIRANTION, JSON.stringify(data))
+    const keyID = req.params.id
+
+    redisClient.hmset(keyID, {
+      'albumId': data.albumId,
+      'id': data.id,
+      'title': data.title,
+      'url': data.url,
+      'thumbnailUrl': data.thumbnailUrl
+     });         
+
+  // sets an expiration date for the data 
+  redisClient.expireat(keyID, parseInt(todayEnd/1000));
 
   res.json(data)
 })
 
-const myPort = process.env.PORT || 3001; // can set PORT to be other num (By the command: set PORT=number)
+const myPort = process.env.PORT || 3005; // can set PORT to be other num (By the command: set PORT=number)
 app.listen(myPort, () => console.log(`Listening on http://localhost:${myPort}`));
