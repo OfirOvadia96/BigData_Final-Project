8d0b8fc498b15ed2b43d50ca9f179e57ede60802
Lioz Akirav <liozakirav@Liozs-MacBook-Pro.local>
2022-04-16 04:00:31 +0300

    sets an expiration date for the data in redis

--- Part-B/models/model.js
+++ Part-B/models/model.js
@@ -3,10 +3,8 @@ const axios = require('axios')
 const cors = require('cors')
 const redisClient = require("./init_redis")
 
-const DEFAULT_EXPIRANTION = 30 // 30 sex
-
-// const todayEnd = new Date().setHours(23, 59, 59, 999);
-// redisClient.expireat("example", parseInt(todayEnd/1000));
+// data expiration time 
+const todayEnd = new Date().setHours(23, 59, 59, 999);
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
@@ -21,8 +19,15 @@ app.get("/photos", async (req, res) => {
     { params: { albumId }}
   )
   
-  redisClient.set('example', "example_changed");
-  // redisClient.setex('photos', DEFAULT_EXPIRANTION, JSON.stringify(data))
+  let keyName = "key"
+
+  redisClient.hmset(keyName, {
+  'javascript': 'ReactJS',
+  'css': 'TailwindCSS',
+  'node': 'Express'
+  });         
+  // sets an expiration date for the data 
+  redisClient.expireat(keyName, parseInt(todayEnd/1000));
 
   res.json(data)
 })
@@ -33,10 +38,10 @@ app.get("/photos/:id", async (req, res) => {
     `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
   )
 
-  redisClient.set('specific', "specific_changed");
+  redisClient.setex('photos', DEFAULT_EXPIRANTION, JSON.stringify(data))
 
   res.json(data)
 })
 
-const myPort = process.env.PORT || 3000; // can set PORT to be other num (By the command: set PORT=number)
+const myPort = process.env.PORT || 3001; // can set PORT to be other num (By the command: set PORT=number)
 app.listen(myPort, () => console.log(`Listening on http://localhost:${myPort}`));
