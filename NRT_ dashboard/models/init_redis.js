b8ddf040a9086533e1402462b6bb11ed364750f6
Lioz Akirav <liozakirav@Liozs-MacBook-Pro.local>
2022-04-19 21:18:18 +0300

    update init_redis to support async functions

--- NRT_ dashboard/models/init_redis.js
+++ NRT_ dashboard/models/init_redis.js
@@ -1,5 +1,9 @@
 const redis = require('redis')
+// Connect to redis at 127.0.0.1 port 6379 no password.
 const client = redis.createClient()
+const {
+    promisify
+} = require('util');
 
 client.on('connect', () => {
     console.log("Client connected to redis...")
@@ -22,5 +26,27 @@ process.on('SIGINT', () => {
 })
 
 
-module.exports = client
+//------------------sets database------------------ 
 
+module.exports.setAsync = promisify(client.set).bind(client);
+module.exports.getAsync = promisify(client.get).bind(client);
+
+// const initDatabase = async () => {
+
+//     const setAsync = promisify(client.set).bind(client);
+//     const getAsync = promisify(client.get).bind(client);
+
+//     await setAsync('waiting', 0);
+
+//     await setAsync('join', 0);
+
+//     await setAsync('complain', 0);
+
+//     await setAsync('ditch', 0);
+
+// };
+
+// initDatabase();
+
+// This dosn't make sense
+module.exports.client = client
