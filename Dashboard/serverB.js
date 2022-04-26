f80d37f51a0f7d9826c6eaf47d1a4001ccc0fd09
Lior <lior4007@gmail.com>
2022-04-26 20:03:08 +0300

    Change design in dashboard

--- Dashboard/serverB.js
+++ Dashboard/serverB.js
@@ -4,10 +4,10 @@ var server = require('http').createServer(app)
 const io = require("socket.io")(server, {
     allowEIO3: true // false by default
 });
-const kafka = require('./models/comsumeKafka');
+// const kafka = require('./models/comsumeKafka');
 const redis = require("./models/redisDB");
 
-const port = 3250
+const port = 3255
 //http://localhost:3250
 
 //--------------Middleware------------------
@@ -40,29 +40,32 @@ io.on("connection", async (socket) => {
 
 });
 
-// ------------Consumer from Kafka-----------------
-kafka.consumer.on("data", async (msg) => {
-    const newCall = JSON.parse(msg.value);
+// // ------------Consumer from Kafka-----------------
+// kafka.consumer.on("data", async (msg) => {
+//     const newCall = JSON.parse(msg.value);
 
-    // **Store the data in Redis and after send to Dashboard */
-    if(String(msg.value).length < 100) //Total wating calls
-    {
-        redis.setTopic('TotalWaiting',parseInt(msg.value));
-    }
-    else if(String(msg.value).includes("topic")) // Details calls
-    {   
-        redis.setTopic(newCall.topic,0);
-        redis.setAverageTime(newCall.totalTime);
-    }
+//     // **Store the data in Redis and after send to Dashboard */
+//     if(String(msg.value).length < 100) //Total wating calls
+//     {
+//         redis.setTopic('TotalWaiting',parseInt(msg.value));
+//     }
+//     else if(String(msg.value).includes("topic")) // Details calls
+//     {   
+//         redis.setTopic(newCall.topic,0);
+//         redis.setAverageTime(newCall.totalTime);
 
-    //Get data from redis to dashboard
-    let allDataArray = await redis.getAllData();
-    let getAverageTime = await redis.getAverageTime();
+//         socket.emit("NewCall", 
+//         {firstname: newCall.firstName, lastname: newCall.lastName, phone: newCall.phone, city: newCall.city, gender: newCall.gender, age: newCall.age, prevcalls: newCall.prevCalls});
+//     }
+
+//     //Get data from redis to dashboard
+//     let allDataArray = await redis.getAllData();
+//     let getAverageTime = await redis.getAverageTime();
     
-    //Send to front with socket
-    io.emit('allData', 
-    {join: allDataArray[0],service: allDataArray[1], complaint: allDataArray[2] , leave: allDataArray[3], waiting: allDataArray[4], averageTotalTime: getAverageTime});
-});
+//     //Send to front with socket
+//     io.emit('allData', 
+//     {join: allDataArray[0],service: allDataArray[1], complaint: allDataArray[2] , leave: allDataArray[3], waiting: allDataArray[4], averageTotalTime: getAverageTime});
+// });
 
 
 
